43fea30994ca1e3d67ed866b29f7b594cf40b27e
christiandeeplearning0828 <christiandeeplearning0828@gmail.com>
2019-12-12 04:29:28 +0000

    update export file url

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://storage.cloud.google.com/cacuna-fastai-deployment'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
